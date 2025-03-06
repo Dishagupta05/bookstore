0592076564cb21b96a138230b5dc7a8df33e3a02
Disha Gupta <dishagupta.9122002@gmail.com>
2025-03-06 23:28:03 +0530

    responsive navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
+import { FaGripLines } from 'react-icons/fa'
 
 const Navbar = () => {
     const links = [
@@ -24,30 +25,53 @@ const Navbar = () => {
             link:"/profile",
         },
     ];
+    const[MobileNav,setMobileNav]=useState("hidden");
   return (
-    <div className='bg-zinc-800 text-white px-8 py-4 flex items-center justify-between'>
+    <>
+    <nav className='z-50 relative bg-zinc-800 text-white px-8 py-4 flex items-center justify-between'>
         <Link to="/" className='flex items-center'>
         <img className='h-10 me-4'
         src="https://cdn-icons-png.flaticon.com/128/10433/10433049.png" 
         alt='logo'></img>
             <h1 className='text-2xl font-semibold'>BookHeaven</h1>
         </Link>
-        <div className='nav-links-bookheaven flex items-center gap-4'>
-            <div className='flex gap-4'>
+        <div className='nav-links-bookheaven block md:flex items-center gap-4'>
+            <div className='hidden md:flex gap-4'>
             {links.map((items,i)=>(
                 <Link to={items.link}
                 className='hover:text-blue-500 transition-300' key={i}>{items.title}
 
                 </Link>))}
             </div>
-            <div className='flex gap-4'>
+            <div className='hidden md:flex gap-4'>
                 <Link to="/LogIn" className='px-2 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>login</Link>
                 <Link to="/SignUp" className='px-2 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300'>Signin</Link>
                            
             </div>
+            <button className='text-white text-2xl hover:text-zinc-400' onClick={()=>MobileNav === "hidden" ? setMobileNav("block") : setMobileNav("hidden")}>
+                <FaGripLines />
+            </button>
         </div>
-    </div>
-  )
-}
+    </nav>
+    <div className={`${MobileNav} bg-zinc-800 h-screen absolute top-0 left-0 w-full z-40 flex flex-col items-center justify-center `}>
+    {links.map((items,i)=>(
+                <Link to={items.link}
+                className={`${MobileNav} text-white text-4xl font:semibold mb-8 hover:text-blue-500 transition-300 duration-300`}
+                key={i}
+                onClick={()=>MobileNav === "hidden"
+                 ? setMobileNav("block") 
+                 :setMobileNav("hidden")}
+                >{items.title}
+
+                </Link>))}
+                
+                <Link to="/LogIn" className={`${MobileNav}px-8 mb-8 text-3xl font-semibold py-2 border border-blue-500 rounded text-white hover:bg-white hover:text-zinc-800 transition-all duration-300`}>login</Link>
+                <Link to="/SignUp" className={`${MobileNav}x-8 mb-8 text-3xl font-semibold py-2 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300`}>Signin</Link>
+                           
+            </div>
+    
+    </>
+  );
+};
 
 export default Navbar
